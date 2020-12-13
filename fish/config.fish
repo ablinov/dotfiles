0a72a2a478552b05dac4e5ceff4bd380aac26211
Alexey Blinov <alexey.blinov@gmail.com>
2020-12-13 16:55:32 +0000

    Add sbin to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,3 @@
 status --is-interactive; and source (rbenv init -|psub)
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
