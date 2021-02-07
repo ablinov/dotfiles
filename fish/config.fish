2b2fff5e888bb5b78caf324dceac67bef2f58e48
Alexey Blinov <alexey.blinov@gmail.com>
2021-02-07 10:42:35 +0000

    Add Android stuff to $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
@@ -5,3 +6,9 @@ status --is-interactive; and source (rbenv init -|psub)
 
 # Update PATH for Google Cloud SDK
 if [ -f '/Users/alexey/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/alexey/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set PATH $PATH $ANDROID_HOME/emulator
+set PATH $PATH $ANDROID_HOME/tools
+set PATH $PATH $ANDROID_HOME/tools/bin
+set PATH $PATH $ANDROID_HOME/platform-tools
