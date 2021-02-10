cf6f57f40899daa56845a4855df7b8a286647009
Alexey Blinov <alexey@songkick.com>
2021-02-10 09:21:07 +0000

    Update Google Cloud SDK location

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,12 @@ set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 status --is-interactive; and source (rbenv init -|psub)
 
-# Update PATH for Google Cloud SDK
-if [ -f '/Users/alexey/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/alexey/Downloads/google-cloud-sdk/path.fish.inc'; end
-
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 set PATH $PATH $ANDROID_HOME/emulator
 set PATH $PATH $ANDROID_HOME/tools
 set PATH $PATH $ANDROID_HOME/tools/bin
 set PATH $PATH $ANDROID_HOME/platform-tools
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/alexey.blinov/Repos/google-cloud-sdk/path.fish.inc' ]; . '/Users/alexey.blinov/Repos/google-cloud-sdk/path.fish.inc'; end
+if [ -f '/Users/alexey/Repos/google-cloud-sdk/path.fish.inc' ]; . '/Users/alexey/Repos/google-cloud-sdk/path.fish.inc'; end
